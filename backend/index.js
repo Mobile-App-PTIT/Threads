3ece02f40260042f0f90b558a9d04842a9445c2d
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-08-17 18:02:15 +0700

    test to use database with backend

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,14 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const compression = require("compression");
 const initWebRoutes = require("./routes/init");
+const User = require("./models/user.model");
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+// app.use(helmet());
 app.use(compression());
 app.use(morgan("dev"));
 
@@ -27,6 +29,32 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: mess, data: data });
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then((result) => {
+    User.findOne()
+      .then((user) => {
+        if (!user) {
+          const user = new User({
+            email: "admin@gmail.com",
+            password: "123456",
+            username: "admin",
+            name: "No Name",
+          });
+          user.save();
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database", err);
+  });
