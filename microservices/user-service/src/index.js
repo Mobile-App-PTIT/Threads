d76ad94b616d47db6b235adaaa1ceed69ba68139
Nguyễn Ngọc Long <51698746+ZNLong2203@users.noreply.github.com>
2024-09-13 12:06:50 +0700

    [BE] init user-service

---
+++ microservices/user-service/src/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerDocs = require('./swagger.json');
+const initWebRoutes = require('./routes/init.route');
+const User = require('./models/user.model');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(morgan('dev'));
+
+// Swagger setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+initWebRoutes(app);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const mess = error.message;
+  const data = error.data;
+  res.status(status).json({ message: mess, data: data });
+});
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then((result) => {
+    User.findOne()
+      .then((user) => {
+        if (!user) {
+          const user = new User({
+            email: 'admin@gmail.com',
+            password: '123456',
+            username: 'admin',
+            name: 'No Name'
+          });
+          user.save();
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    });
+  })
+  .catch((err) => {
+    console.log('Cannot connect to the database', err);
+  });
