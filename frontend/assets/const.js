0e0b795cf942e439b4fd885b307cba77f2213b8b
TLam2908 <buitunglam2003@gmail.com>
2024-10-30 16:51:20 +0700

    message dummy data

---
+++ frontend/assets/const.js
@@ -0,0 +1,39 @@
+const background = require("./avatar/background.jpeg");
+const background2 = require("./avatar/background2.jpeg");
+const girl1 = require("./avatar/girl1.png");
+const girl2 = require("./avatar/girl2.png");
+const guy1 = require("./avatar/guy1.png");
+const guy2 = require("./avatar/guy2.png");
+const logo = require("./avatar/logo.png");
+const user1 = require("./avatar/user1.jpeg");
+const user2 = require("./avatar/user2.jpeg");
+const user3 = require("./avatar/user3.jpeg");
+const user4 = require("./avatar/user4.jpeg");
+const user5 = require("./avatar/user5.jpeg");
+const user6 = require("./avatar/user6.jpeg");
+const user7 = require("./avatar/user7.jpeg");
+const user8 = require("./avatar/user8.jpeg");
+const user9 = require("./avatar/user9.jpeg");
+const user10 = require("./avatar/user10.jpeg");
+const user11 = require("./avatar/user11.jpeg");
+
+export default {
+    background,
+    background2,
+    girl1,
+    girl2,
+    guy1,
+    guy2,
+    logo,
+    user1,
+    user2,
+    user3,
+    user4,
+    user5,
+    user6,
+    user7,
+    user8,
+    user9,
+    user10,
+    user11
+}
--- frontend/src/screens/ListMessageScreen.jsx
+++ frontend/src/screens/ListMessageScreen.jsx
@@ -1,10 +1,82 @@
-import { View, Text } from 'react-native';
-const ListMessageScreen = () => {
-    return (
-        <View className='bg-zinc-900 flex-1'>
-            <Text className='text-white'>ListMessageScreen</Text>
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+  Image,
+  FlatList,
+} from 'react-native';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
+import {useState} from 'react';
+import {messsagesData} from '../../assets';
+
+const ListMessageScreen = ({navigation}) => {
+  const [search, setSearch] = useState('');
+
+  const renderItem = ({item, index}) => {
+    console.log('item', item);
+    return (<TouchableOpacity
+      key={index}
+      onPress={() => navigation.navigate('Chat', {userName: item.fullName})}>
+      {/*   style={[styles.userContainer, index % 2 !== 0 ? styles.background: null]} */}
+      <View>
+        {item.isOnline && item.isOnline === true && <View></View>}
+        <Image source={item.userImg} resizeMode="contain" />
+      </View>
+      <View className="flex-row">
+        <View>
+          <Text className='text-white'>{item.fullName}</Text>
+          <Text className='text-white'>{item.lastMessage}</Text>
         </View>
+
+        <View className="absolute right-[4px] items-center">
+          <Text>{item.lastMessageTime}s</Text>
+        </View>
+      </View>
+
+      <TouchableOpacity className="w-5 h-5 rounded-xl justify-center items-center mt-[12px]">
+        <Text className='text-white'>{item.messageInQueue}</Text>
+      </TouchableOpacity>
+    </TouchableOpacity>
     )
-}
+  };
+
+  const renderContent = () => {
+    console.log('messsagesData', messsagesData);
+    return (
+      <View>
+        <View className="flex-row items-center bg-neutral-800 h-18 w-[380px] my-[15px] px-[15px] rounded-xl">
+          <TouchableOpacity>
+            <Ionicon name="search" size={26} color="gray" />
+          </TouchableOpacity>
+          <TextInput
+            placeholder="Search"
+            placeholderTextColor="gray"
+            className="flex-1 h-[100%] mx-[8px]"
+          />
+          <TouchableOpacity>
+            <Feather name="edit" size={24} color="gray" />
+          </TouchableOpacity>
+        </View>
+        {/* Render chat */}
+        <View>
+          <FlatList
+            data={messsagesData}
+            showsVerticalScrollIndicator={false}
+            renderItem={renderItem}
+            keyExtractor={item => item.id.toString()}></FlatList>
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView className="flex-1 bg-zinc-900 text-white">
+      <View className="p-5 items-center">{renderContent()}</View>
+    </SafeAreaView>
+  );
+};
 
 export default ListMessageScreen;
