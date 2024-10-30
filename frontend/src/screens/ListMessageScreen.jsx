690799a4387d031d73897a88b0824ef8ecfa6dfd
TLam2908 <buitunglam2003@gmail.com>
2024-10-30 23:13:55 +0700

    Update ListMessageScreen.jsx

--- frontend/src/screens/ListMessageScreen.jsx
+++ frontend/src/screens/ListMessageScreen.jsx
@@ -6,45 +6,67 @@ import {
   TextInput,
   Image,
   FlatList,
+  ScrollView,
 } from 'react-native';
 import Ionicon from 'react-native-vector-icons/Ionicons';
 import Feather from 'react-native-vector-icons/Feather';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {messsagesData} from '../../assets';
 
 const ListMessageScreen = ({navigation}) => {
   const [search, setSearch] = useState('');
+  const [data, setData] = useState(messsagesData);
+
+  
+  const handleSearch = text => {
+    setSearch(text);
+    if (text.length > 0) {
+      const filteredData = messsagesData.filter((item) => {
+        return item.fullName.toLowerCase().includes(text.toLowerCase());
+      })
+      console.log(messsagesData)
+      console.log(filteredData);
+      setData(filteredData);
+    } else {
+      setData(messsagesData);
+    }
+  }
 
   const renderItem = ({item, index}) => {
-    console.log('item', item);
-    return (<TouchableOpacity
-      key={index}
-      onPress={() => navigation.navigate('Chat', {userName: item.fullName})}>
-      {/*   style={[styles.userContainer, index % 2 !== 0 ? styles.background: null]} */}
-      <View>
-        {item.isOnline && item.isOnline === true && <View></View>}
-        <Image source={item.userImg} resizeMode="contain" />
-      </View>
-      <View className="flex-row">
-        <View>
-          <Text className='text-white'>{item.fullName}</Text>
-          <Text className='text-white'>{item.lastMessage}</Text>
+    return (
+      <TouchableOpacity
+        className="flex-row w-full items-center justify-between"
+        key={index}
+        onPress={() => navigation.navigate('Chat', {userName: item.fullName})}>
+        {/*   style={[styles.userContainer, index % 2 !== 0 ? styles.background: null]} */}
+        <View className="py-4">
+          {item.isOnline && item.isOnline === true && (
+            <View className="h-5 w-5 rounded-full bg-green-500 absolute top-4 right-0 z-50 border border-white"></View>
+          )}
+          <Image
+            source={item.userImg}
+            resizeMode="contain"
+            className="h-20 w-20 rounded-full"
+          />
         </View>
-
-        <View className="absolute right-[4px] items-center">
-          <Text>{item.lastMessageTime}s</Text>
+        <View className="flex-row w-[280px]">
+          <View className="flex-col">
+            <Text className="text-gray-300 font-semibold mb-1 text-[14px]">
+              {item.fullName}
+            </Text>
+            <Text className="text-gray-500">{item.lastMessage}</Text>
+          </View>
+          <View className="absolute right-[4px] items-center">
+            <Text className="text-white text-[12px]">
+              {item.lastMessageTime}
+            </Text>
+          </View>
         </View>
-      </View>
-
-      <TouchableOpacity className="w-5 h-5 rounded-xl justify-center items-center mt-[12px]">
-        <Text className='text-white'>{item.messageInQueue}</Text>
       </TouchableOpacity>
-    </TouchableOpacity>
-    )
+    );
   };
 
   const renderContent = () => {
-    console.log('messsagesData', messsagesData);
     return (
       <View>
         <View className="flex-row items-center bg-neutral-800 h-18 w-[380px] my-[15px] px-[15px] rounded-xl">
@@ -52,9 +74,12 @@ const ListMessageScreen = ({navigation}) => {
             <Ionicon name="search" size={26} color="gray" />
           </TouchableOpacity>
           <TextInput
+            onChangeText={handleSearch}
+            
+            value={search}
             placeholder="Search"
             placeholderTextColor="gray"
-            className="flex-1 h-[100%] mx-[8px]"
+            className="flex-1 h-[100%] mx-[8px] text-white"
           />
           <TouchableOpacity>
             <Feather name="edit" size={24} color="gray" />
@@ -63,7 +88,7 @@ const ListMessageScreen = ({navigation}) => {
         {/* Render chat */}
         <View>
           <FlatList
-            data={messsagesData}
+            data={data}
             showsVerticalScrollIndicator={false}
             renderItem={renderItem}
             keyExtractor={item => item.id.toString()}></FlatList>
@@ -73,8 +98,10 @@ const ListMessageScreen = ({navigation}) => {
   };
 
   return (
-    <SafeAreaView className="flex-1 bg-zinc-900 text-white">
-      <View className="p-5 items-center">{renderContent()}</View>
+    <SafeAreaView className="flex-1 h-full bg-zinc-900 text-white">
+      <ScrollView>
+        <View className="p-5 items-center">{renderContent()}</View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
