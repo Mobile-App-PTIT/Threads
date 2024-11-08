e607d82d29f3b220042ce74711ebb8878d78dac6
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-11-08 21:30:26 +0700

    Update user.route.js

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,19 +1,22 @@
-const express = require("express");
-const userController = require("../controllers/user.controller");
-const { upload } = require("../configs/cloudinary");
-const { isAuth } = require("../middleware/privilege");
+const express = require('express');
+const userController = require('../controllers/user.controller');
+const { upload } = require('../configs/cloudinary');
+const { isAuth } = require('../middleware/privilege');
 
 const router = express.Router();
 
-router.get("/:user_id", userController.getUserInfo);
-router.get("/", userController.getAllUserInfo);
-router.patch("/:user_id", isAuth, upload.single('avatar'), userController.updateUserInfo);
-router.delete("/:user_id", isAuth, userController.deleteUser);
+// get user's messages
+router.get('/messages', isAuth, userController.getUserFollowerAndFollowing);
+
+router.get('/:user_id', userController.getUserInfo);
+router.get('/', userController.getAllUserInfo);
+router.patch('/:user_id', isAuth, upload.single('avatar'), userController.updateUserInfo);
+router.delete('/:user_id', isAuth, userController.deleteUser);
 
 // Followers and Following
-router.get("/replied/:user_id", userController.getUserReplied);
-router.get("/followers/:user_id", userController.getUserFollowers);
-router.get("/following/:user_id", userController.getUserFollowing);
-router.patch("/follow/:follower_id", isAuth, userController.FollowOrUnfollowUser);
+router.get('/replied/:user_id', userController.getUserReplied);
+router.get('/followers/:user_id', userController.getUserFollowers);
+router.get('/following/:user_id', userController.getUserFollowing);
+router.patch('/follow/:follower_id', isAuth, userController.FollowOrUnfollowUser);
 
 module.exports = router;
