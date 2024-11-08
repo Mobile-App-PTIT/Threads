20d9e17bb8b12489cf4c67bfc06df1b2f5c09b3d
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-11-08 21:31:00 +0700

    Update store.js

--- frontend/redux/store.js
+++ frontend/redux/store.js
@@ -1,17 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './reducers/userReducer';
 import { postReducer } from './reducers/postReducer';
+import { messageReducer } from './reducers/messageReducer';
 
 const Store = configureStore({
-    reducer:{
-        user: userReducer,
-        post: postReducer,
-        // notification: notificationReducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-        immuatbleCheck: false,
-        serializableCheck: false,
-    })
-})
+  reducer: {
+    user: userReducer,
+    post: postReducer,
+    message: messageReducer
+    // notification: notificationReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    immuatbleCheck: false,
+    serializableCheck: false
+  })
+});
 
-export default Store
+export default Store;
