3a1d31cefb332e10171f81db776cf2e141252764
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-11-08 21:30:14 +0700

    Update message.model.js

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -3,30 +3,30 @@ const { Schema, model, Types } = mongoose;
 
 
 const messageSchema = new Schema({
-    type: {
-        type: String,
-        enum: ['text', 'image', 'audio'],
-        required: true,
-    },
-    message: {
-        type: String,
-        required: true,
-    },
-    status: {
-        type: String,
-        enum: ['sent', 'delivered', 'read'],
-        required: true,
-    },
-    sender_id: {
-        type: Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    receiver_id: {
-        type: Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-})
+  type: {
+    type: String,
+    enum: ['text', 'image', 'audio', 'file'],
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['sent', 'delivered', 'read'],
+    required: true
+  },
+  sender_id: {
+    type: Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  receiver_id: {
+    type: Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
 
 module.exports = model('Message', messageSchema);
