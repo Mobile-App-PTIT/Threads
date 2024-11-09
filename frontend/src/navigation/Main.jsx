08ef29c6ddd127c256afa0dee760755129ae0d30
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-11-09 15:13:43 +0700

    Update Main.jsx

--- frontend/src/navigation/Main.jsx
+++ frontend/src/navigation/Main.jsx
@@ -10,9 +10,7 @@ const Main = () => {
   const Stack = createNativeStackNavigator();
   return (
     <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-
       <Stack.Screen name="Home" component={Tabs} />
-
       {/* <Stack.Screen name="HomeScreen" component={HomeScreen} /> */}
       <Stack.Screen name="EditProfile" component={EditProfile} />
       <Stack.Screen name="PostDetailScreen" component={PostDetailScreen} />
