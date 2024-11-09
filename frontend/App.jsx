75140f802dfb3f3930a9fe8b8d4f083ae8f76ef8
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-11-09 10:02:12 +0700

    change socketcontext wrap position

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,38 +1,40 @@
 import './global.css';
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import { Text } from 'react-native';
 import Auth from './src/navigation/Auth';
 import Main from './src/navigation/Main';
 import Store from './redux/store';
-import {Provider, useSelector} from 'react-redux';
-import {NavigationContainer} from '@react-navigation/native';
-import { loadUser } from './redux/actions/userAction';
-import Toast from 'react-native-toast-message'; // Import Toast
+import { Provider, useSelector } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import Toast from 'react-native-toast-message';
+import { SocketProvider } from './src/components/SocketContext'; // Import Toast
 
 function App() {
   return (
     <Provider store={Store}>
       <AppStack />
-      <Toast/>
+      <Toast />
     </Provider>
   );
 }
 
 const AppStack = () => {
   const [isLogin, setIsLogin] = useState(true);
-  const {isAuthenticated, isLoading} = useSelector(state => state.user);
+  const { isAuthenticated, isLoading } = useSelector(state => state.user);
   const [isLoading1, setIsLoading1] = useState(false);
 
   return (
     <>
       {isLoading ? (
-        <Text className='text-black'>Loading</Text>
+        <Text className="text-black">Loading</Text>
       ) : (
         <>
           {isAuthenticated ? ( // sau thay = isAuthenticated
-            <NavigationContainer>
-              <Main/>
-            </NavigationContainer>
+            <SocketProvider>
+              <NavigationContainer>
+                <Main />
+              </NavigationContainer>
+            </SocketProvider>
           ) : (
             <NavigationContainer>
               <Auth />
@@ -40,7 +42,7 @@ const AppStack = () => {
           )}
         </>
       )}
-      
+
     </>
   );
 };
--- frontend/src/navigation/Main.jsx
+++ frontend/src/navigation/Main.jsx
@@ -1,26 +1,27 @@
-import HomeScreen from "../screens/HomeScreen";
-import PostDetailScreen from "../screens/PostDetailScreen";
-import EditProfile from "../components/EditProfile"
-import ListMessageScreen from "../screens/ListMessageScreen";
-import ReplyDetailsScreen from "../screens/ReplyDetailsScreen";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import ChatScreen from "../screens/ChatScreen";
-import Tabs from "./Tabs";
+import PostDetailScreen from '../screens/PostDetailScreen';
+import EditProfile from '../components/EditProfile';
+import ListMessageScreen from '../screens/ListMessageScreen';
+import ReplyDetailsScreen from '../screens/ReplyDetailsScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ChatScreen from '../screens/ChatScreen';
+import Tabs from './Tabs';
 
 const Main = () => {
-    const Stack = createNativeStackNavigator();
-    return (
-        <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
-            <Stack.Screen name="Home" component={Tabs}/>
-            {/* <Stack.Screen name="HomeScreen" component={HomeScreen} /> */}
-            <Stack.Screen name="EditProfile" component={EditProfile} />
-            <Stack.Screen name="PostDetailScreen" component={PostDetailScreen} />
-            <Stack.Screen name="ReplyDetailsScreen" component={ReplyDetailsScreen} />
-            <Stack.Screen name="ListMessageScreen" component={ListMessageScreen} />
-            <Stack.Screen name="ChatScreen" component={ChatScreen} />
-            
-        </Stack.Navigator>
-    )
-}
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+
+      <Stack.Screen name="Home" component={Tabs} />
+
+      {/* <Stack.Screen name="HomeScreen" component={HomeScreen} /> */}
+      <Stack.Screen name="EditProfile" component={EditProfile} />
+      <Stack.Screen name="PostDetailScreen" component={PostDetailScreen} />
+      <Stack.Screen name="ReplyDetailsScreen" component={ReplyDetailsScreen} />
+      <Stack.Screen name="ListMessageScreen" component={ListMessageScreen} />
+      <Stack.Screen name="ChatScreen" component={ChatScreen} />
+
+    </Stack.Navigator>
+  );
+};
 
 export default Main;
