a7293465b85e35783eecd012e3bff75926299cba
TLam2908 <buitunglam2003@gmail.com>
2024-11-09 16:35:24 +0700

    fix app.js

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -7,43 +7,33 @@ import Store from './redux/store';
 import { Provider, useSelector } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import Toast from 'react-native-toast-message';
-import { SocketProvider } from './src/components/SocketContext'; // Import Toast
+import { SocketProvider } from './src/components/SocketContext';
 
 function App() {
   return (
     <Provider store={Store}>
-      <AppStack />
-      <Toast />
+      <NavigationContainer>
+        <AppStack />
+        <Toast />
+      </NavigationContainer>
     </Provider>
   );
 }
 
 const AppStack = () => {
-  const [isLogin, setIsLogin] = useState(true);
   const { isAuthenticated, isLoading } = useSelector(state => state.user);
-  const [isLoading1, setIsLoading1] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
 
-  return (
-    <>
-      {isLoading ? (
-        <Text className="text-black">Loading</Text>
-      ) : (
-        <>
-          {isAuthenticated ? ( // sau thay = isAuthenticated
-            <SocketProvider>
-              <NavigationContainer>
-                <Main />
-              </NavigationContainer>
-            </SocketProvider>
-          ) : (
-            <NavigationContainer>
-              <Auth />
-            </NavigationContainer>
-          )}
-        </>
-      )}
+  if (isLoading) {
+    return <Text className="text-black">Loading</Text>;
+  }
 
-    </>
+  return isAuthenticated ? (
+    <SocketProvider>
+      <Main />
+    </SocketProvider>
+  ) : (
+    <Auth />
   );
 };
 
