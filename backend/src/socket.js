53199263420ece62ec3a8aeb4797c67d66a56cc6
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-11-12 13:17:16 +0700

    add socket to get list followers and following

--- backend/src/socket.js
+++ backend/src/socket.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const socketIo = require('socket.io');
 const redisClient = require('./configs/redis');
+const User = require('./models/user.model');
+const Message = require('./models/message.model');
 
 let socketServer = (app) => {
   const server = http.createServer(app);
@@ -22,6 +24,67 @@ let socketServer = (app) => {
           await redisClient.subscribe(`notifications:${userId}`, (message) => {
             socket.emit('notification', message);
           });
+        }
+      }
+    });
+
+    // getFollowingAndFollowers event, receive accessToken
+    socket.on('getList', async (accessToken) => {
+      const userId = await redisClient.get(`accessToken:${accessToken}`);
+      if (userId) {
+        const user_id = JSON.parse(userId);
+        if (!userSocketMap.has(user_id)) {
+          userSocketMap.set(user_id, socket.id);
+
+          const user = await User.findById(user_id)
+            .select('following followers')
+            .populate('following followers', 'name avatar');
+
+          // get last message of each user in following and followers
+          const followingMessages = await Message.find({
+            $or: [
+              { sender: { $in: user.following } },
+              { receiver: { $in: user.following } }
+            ]
+          }).sort({ createdAt: -1 }).limit(1);
+          const followerMessages = await Message.find({
+            $or: [
+              { sender: { $in: user.followers } },
+              { receiver: { $in: user.followers } }
+            ]
+          }).sort({ createdAt: -1 }).limit(1);
+
+          const data = [];
+
+          for (const u of user.following) {
+            const lastMessage = followingMessages.find(m => m.sender.toString() === u._id.toString() || m.receiver.toString() === u._id.toString());
+            data.push({
+              id: u._id,
+              fullName: u.name,
+              isOnline: userSocketMap.has(u._id),
+              userImg: u.avatar,
+              lastSeen: lastMessage?.seenAt || '',
+              lastMessage: lastMessage?.content || '',
+              lastMessageTime: lastMessage?.createdAt || ''
+            });
+          }
+
+          for (const u of user.followers) {
+            const lastMessage = followerMessages.find(m => m.sender.toString() === u._id.toString() || m.receiver.toString() === u._id.toString());
+            data.push({
+              id: u._id,
+              fullName: u.name,
+              isOnline: userSocketMap.has(u._id),
+              userImg: u.avatar,
+              lastSeen: lastMessage?.seenAt || '',
+              lastMessage: lastMessage?.content || '',
+              lastMessageTime: lastMessage?.createdAt || ''
+            });
+          }
+
+          // console.log(data);
+
+          socket.emit('getFollowingAndFollowers', data);
         }
       }
     });
