e089c96c4194714bbeeb4ad6394c830be6408247
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-11-12 13:17:32 +0700

    Update SocketContext.jsx

--- frontend/src/components/SocketContext.jsx
+++ frontend/src/components/SocketContext.jsx
@@ -1,14 +1,17 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { AppState } from 'react-native';
 import io from 'socket.io-client';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
+  const dispatch = useDispatch();
+  const { users } = useSelector(state => state.message);
 
   useEffect(() => {
-    const newSocket = io('http://localhost:3000');
+    const newSocket = io('http://10.0.2.2:3000');
     setSocket(newSocket);
 
     newSocket.on('notification', (message) => {
@@ -16,8 +19,15 @@ export const SocketProvider = ({ children }) => {
       // Handle the notification message as needed
     });
 
+    newSocket.on('getFollowingAndFollowers', (data) => {
+      if (JSON.stringify(data) !== JSON.stringify(users)) {
+        dispatch({ type: 'getFollowingAndFollowersSuccess', payload: data });
+      }
+    });
+
     const handleAppStateChange = (nextAppState) => {
       if (nextAppState === 'inactive' || nextAppState === 'background') {
+        newSocket.emit('disconnect');
         newSocket.close();
       }
     };
@@ -26,6 +36,7 @@ export const SocketProvider = ({ children }) => {
 
     return () => {
       newSocket.off('notification');
+      newSocket.off('getFollowingAndFollowers');
       newSocket.close();
       subscription.remove();
     };
