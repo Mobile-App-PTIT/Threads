87a4fe0d942dc9c9f5a6c80c0a68780758187af5
Nguyễn Ngọc Long <51698746+ZNLong2203@users.noreply.github.com>
2024-11-18 15:12:28 +0700

    fix: hot fix

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -112,23 +112,15 @@ const getUserReplied = async (req, res, next) => {
     const { user_id } = req.params;
 
     const replies = await Reply.find({
-      user_id
-    }).populate({
-      'path': 'user_id',
-      'select': '_id name avatar'
-    }).populate({
-      'path': 'post_id',
-      'populate': {
-        'path': 'user_id',
-        'select': '_id name avatar'
-      }
+      user_id,
+      replies: { $exists: true, $not: { $size: 0 } }
     }).populate({
       'path': 'replies',
       'populate': {
         'path': 'user_id',
         'select': '_id name avatar'
       }
-    }).sort({ createdAt: -1 }).lean();
+    }).populate('user_id', '_id name avatar').sort({ createdAt: -1 }).lean();
 
     res.status(200).json({
       message: 'User replies fetched successfully',
