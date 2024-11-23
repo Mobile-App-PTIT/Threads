ab0649bc804377ffa2f60f529ab15203ef404110
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-11-23 08:26:30 +0700

    Update user.model.js

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,56 +1,60 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { Schema, model, Types } = mongoose;
 
 const userSchema = new Schema({
   subname: {
     type: String,
-    required: false,
+    required: false
   },
   password: {
     type: String,
-    required: true,
+    required: true
   },
   name: {
     type: String,
-    required: true,
+    required: true
   },
   bio: {
-    type: String,
+    type: String
     // required: true,
   },
   email: {
     type: String,
-    required: true,
+    required: true
   },
   avatar: {
     type: String,
-    default: "https://inkythuatso.com/uploads/thumbnails/800/2023/03/9-anh-dai-dien-trang-inkythuatso-03-15-27-03.jpg",
+    default: 'https://inkythuatso.com/uploads/thumbnails/800/2023/03/9-anh-dai-dien-trang-inkythuatso-03-15-27-03.jpg'
     // required: true,
+  },
+  fcmToken: {
+    type: String,
+    default: ''
   },
   followers: {
     type: [Types.ObjectId],
-    ref: "User",
-    default: [],
+    ref: 'User',
+    default: []
   },
   following: {
     type: [Types.ObjectId],
-    ref: "User",
-    default: [],
+    ref: 'User',
+    default: []
   },
   share: [ // Array of post with status
     {
       post_id: {
         type: Types.ObjectId,
-        ref: "Post",
-        required: true,
+        ref: 'Post',
+        required: true
       },
       status: {
         type: String,
-        enum: ["public", "private"],
-        default: "public",
+        enum: ['public', 'private'],
+        default: 'public'
       }
     }
   ]
 });
 
-module.exports = model("User", userSchema);
+module.exports = model('User', userSchema);
